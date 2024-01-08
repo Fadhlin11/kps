e52e530e37b83915a4a44a0131cdc9c83a1933ad
Fadhlin11 <sakinakhalid11@gmail.com>
2024-01-09 01:44:05 +0800

    Update content.blade.php

--- resources/views/admin/layouts/content.blade.php
+++ resources/views/admin/layouts/content.blade.php
@@ -9,7 +9,7 @@
                                     <div class="widget-body">
                                         <div class="d-flex justify-content-between align-items-center">
                                             <div class="state">
-                                                <h6>Patients</h6>
+                                                <h6>Number of Patients</h6>
                                                 <h2>{{App\Models\User::where('user_type',3)->count()}}</h2>
                                             </div>
                                             <div class="icon">
@@ -18,9 +18,7 @@
                                         </div>
                                         <small class="text-small mt-10 d-block"></small>
                                     </div>
-                                    <div class="progress progress-sm">
-                                        <div class="progress-bar bg-danger" role="progressbar" aria-valuenow="62" aria-valuemin="0" aria-valuemax="100" style="width: 62%;"></div>
-                                    </div>
+                                   
                                 </div>
                             </div>
                             <div class="col-lg-4 col-md-6 col-sm-12">
@@ -28,18 +26,16 @@
                                     <div class="widget-body">
                                         <div class="d-flex justify-content-between align-items-center">
                                             <div class="state">
-                                                <h6>Dentist</h6>
+                                                <h6>Number of Dentists</h6>
                                                 <h2>{{App\Models\User::where('user_type',1)->count()}}</h2>
                                             </div>
                                             <div class="icon">
-                                                <i class="ik ik-user-plus"></i>
+                                                <i class="ik ik-users"></i>
                                             </div>
                                         </div>
                                         <small class="text-small mt-10 d-block"></small>
                                     </div>
-                                    <div class="progress progress-sm">
-                                        <div class="progress-bar bg-success" role="progressbar" aria-valuenow="78" aria-valuemin="0" aria-valuemax="100" style="width: 78%;"></div>
-                                    </div>
+                                   
                                 </div>
                             </div>
                             <div class="col-lg-4 col-md-6 col-sm-12">
@@ -53,11 +49,7 @@
                                             <div class="icon">
                                                 <i class="ik ik-user-check"></i>
                                             </div>
-                                        </div>
-                                       
-                                    </div>
-                                    <div class="progress progress-sm">
-                                        <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="31" aria-valuemin="0" aria-valuemax="100" style="width: 31%;"></div>
+                                        </div>  
                                     </div>
                                 </div>
                             </div>
@@ -66,17 +58,14 @@
                                     <div class="widget-body">
                                         <div class="d-flex justify-content-between align-items-center">
                                             <div class="state">
-                                                <h6>Appointments</h6>
+                                                <h6>Number of Appointments</h6>
                                                 <h2>{{App\Models\Booking::count()}}</h2>
                                             </div>
                                             <div class="icon">
-                                                <i class="ik ik-message-square"></i>
+                                                <i class="ik ik-calendar"></i>
                                             </div>
                                         </div>
                                        
-                                    </div>
-                                    <div class="progress progress-sm">
-                                        <div class="progress-bar bg-info" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%;"></div>
                                     </div>
                                 </div>
                             </div>
@@ -94,9 +83,6 @@
                                             </div>
                                         </div>
                                        
-                                    </div>
-                                    <div class="progress progress-sm">
-                                        <div class="progress-bar bg-info" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%;"></div>
                                     </div>
                                 </div>
                             </div>
